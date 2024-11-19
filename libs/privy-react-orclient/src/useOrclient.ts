ce265634b8664ea74203e00d90ec2568445101af
Tadas <vtadas25@gmail.com>
2024-11-19 15:10:25 +0200

    privy-orclient: add optional argument for orclient config

--- libs/privy-react-orclient/src/useOrclient.ts
+++ libs/privy-react-orclient/src/useOrclient.ts
@@ -1,20 +1,22 @@
 import { useEffect, useState } from "react";
 import { DeploymentSpec, createOrclient } from "@ordao/orclient/createOrclient.js";
-import { ORClient } from "@ordao/orclient";
+import { ORClient, Config } from "@ordao/orclient";
 import { ConnectedWallet } from "@privy-io/react-auth";
 
 async function create(
   deployment: DeploymentSpec,
-  wallet: ConnectedWallet
+  wallet: ConnectedWallet,
+  orclientCfg?: Config
 ): Promise<ORClient> {
   const provider = await wallet.getEthereumProvider();
-  const orclient = await createOrclient(deployment, provider);
+  const orclient = await createOrclient(deployment, provider, orclientCfg);
   return orclient;
 }
 
 export function useOrclient(
   deployment?: DeploymentSpec,
-  wallet?: ConnectedWallet
+  wallet?: ConnectedWallet,
+  orclientConfig?: Config
 ) {
   const [orclient, setOrclient] = useState<ORClient | undefined>(undefined);
 
@@ -23,7 +25,7 @@ export function useOrclient(
     console.debug = console.log;
 
     if (deployment && wallet) {
-      create(deployment, wallet).then(
+      create(deployment, wallet, orclientConfig).then(
         orclient => setOrclient(orclient),
         err => console.error(err)
       );
