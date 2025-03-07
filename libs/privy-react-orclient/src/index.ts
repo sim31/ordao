ac1b7b1fed7a3730f6df387bc17f32b81326e7da
Tadas <vtadas25@gmail.com>
2025-03-07 10:12:30 +0200

    Export deployments object through privy-react-orclient

--- libs/privy-react-orclient/src/index.ts
+++ libs/privy-react-orclient/src/index.ts
@@ -3,3 +3,5 @@ export * from "./useOrclient.js";
 
 export * from "./version.js";
 export * from "./libVersions.js";
+
+export { DeploymentKey, DeploymentSpec, deployments } from "@ordao/orclient/createOrclient.js";
