e579b5200b22e47c25c3fca905f065659fa9c052
Tadas <vtadas25@gmail.com>
2025-05-26 16:32:35 +0300

    Add block explorer link

--- apps/gui/src/routes/_app.ef/awards.tsx
+++ apps/gui/src/routes/_app.ef/awards.tsx
@@ -1,8 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { assertOrclientBeforeLoad } from '../../global/routerContext'
-import { Table } from '@chakra-ui/react';
-import { z } from 'zod';
+import { Link, Table, VStack } from '@chakra-ui/react';
 import { isValidHttpUrl } from '../../utils/isUrl';
+import { config } from '../../global/config';
 
 export const Route = createFileRoute('/_app/ef/awards')({
   component: RouteComponent,
@@ -19,27 +19,31 @@ function RouteComponent() {
   const { awards } = Route.useLoaderData();
 
   return (
-    <Table.Root variant="outline">
-      <Table.Header>
-        <Table.Row>
-          <Table.ColumnHeader fontWeight="bold">Title</Table.ColumnHeader>
-          <Table.ColumnHeader fontWeight="bold">Reason</Table.ColumnHeader>
-          <Table.ColumnHeader fontWeight="bold">Denomination</Table.ColumnHeader>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        {awards.map(award => (
-          <Table.Row key={award.properties.tokenId}>
-            <Table.Cell>{award.properties.title}</Table.Cell>
-            <Table.Cell wordBreak="break-word">{
-              isValidHttpUrl(award.properties.reason ?? "")
-              ? <a target="_blank" href={award.properties.reason}>{award.properties.reason}</a>
-              : award.properties.reason
-            }</Table.Cell>
-            <Table.Cell>{award.properties.denomination}</Table.Cell>
+    <VStack gap="1em" alignItems={"flex-end"}>
+      <Link href={config.childRespectLink} target="_blank">Block Explorer</Link>  
+      <Table.Root variant="outline">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeader fontWeight="bold">Title</Table.ColumnHeader>
+            <Table.ColumnHeader fontWeight="bold">Reason</Table.ColumnHeader>
+            <Table.ColumnHeader fontWeight="bold">Denomination</Table.ColumnHeader>
           </Table.Row>
-        ))}
-      </Table.Body>
-    </Table.Root>
+        </Table.Header>
+        <Table.Body>
+          {awards.map(award => (
+            <Table.Row key={award.properties.tokenId}>
+              <Table.Cell>{award.properties.title}</Table.Cell>
+              <Table.Cell wordBreak="break-word">{
+                isValidHttpUrl(award.properties.reason ?? "")
+                ? <a target="_blank" href={award.properties.reason}>{award.properties.reason}</a>
+                : award.properties.reason
+              }</Table.Cell>
+              <Table.Cell>{award.properties.denomination}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+
+    </VStack>
   )
 }
